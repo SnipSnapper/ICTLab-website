6b64eebf2b8e316f37c9297dfd65470db936a55c
SnipSnapper <ceesvdtoren@gmail.com>
2018-04-02 15:19:00 +0200

    first commit
    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,5 @@
+@extends("layout.layout")
+
+@section("content")
+<h1>This is the login page</h1>
+@endsection
---
+++ resources/views/member.blade.php
@@ -0,0 +1,5 @@
+@extends("layout.layout")
+
+@section("content")
+<h1>you are at the member page</h1>
+@endsection
